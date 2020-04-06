22b09c26c9bb1a014cfe88156970e9c04f8f0afa
본점 403-29 < 403-29@DESKTOP-H3NI4LK>
2020-04-06 18:21:58 +0900

    4/6 paging

--- JSP06/WebContent/list.jsp
+++ JSP06/WebContent/list.jsp
@@ -13,14 +13,17 @@
 	<fmt:requestEncoding value="utf-8"/>
 	<jsp:useBean id="dao" class="testBoard.TestDAO"/>
 	<c:set var="listDto" value="${dao.list() }"/>
-	<%-- --%>
+	
+	<%-- 페이징 --%>
+	<c:set var="totalPage" value="${dao.getTotalPage() }"/>	
+	
 	<table border="1">
 		<tr><th>번호</th><th>제목</th><th>등록날짜</th><th>조회수</th></tr>
 			<c:choose>
 				<c:when test="${listDto.size() != 0 }">
 					<c:forEach var="l" items="${listDto }">
 						<tr>
-							<td>${l.num }</td><td>${l.title }</td>
+							<td>${l.num }</td><td><a href="count.jsp?num=${l.num }">${l.title }</a></td>
 							<td>${l.pdate }</td><td>${l.count }</td>
 						</tr>
 					</c:forEach>
@@ -29,7 +32,46 @@
 					<tr><th colspan="4">등록된 정보가 없습니다</th></tr>
 				</c:otherwise>
 			</c:choose>
-			<tr><td colspan="4" align="right"><button type="button" onclick="location.href='regForm.jsp'">등록</button></td></tr>
+			
+			<%-- 페이징 --%>
+			<tr><td colspan="4" align="right">
+				<c:choose>
+					<c:when test="${param.start == null }">	<%--페이지를 처음 들어온거면 1페이지로 --%>
+						<c:set var="start" value="1"/>
+					</c:when>
+					<c:otherwise>
+						<c:set var="start" value="${param.start }"/>	<%--특정 페이지로 들어온거면 그 페이지로 --%>
+					</c:otherwise>
+				</c:choose>
+				
+				<%--이전 버튼. 현 페이지에서 -1. 1이면 버튼 비활성화 --%>
+				<c:choose>
+					<c:when test="${start > 1 }">
+						<button type="button" onclick="location.href='list.jsp?start=${start-1}'">이전</button>
+					</c:when>
+					<c:otherwise>
+						<button type="button" disabled="disabled">이전</button>
+					</c:otherwise>
+				</c:choose>
+				
+				<%--페이지 목록 --%>
+				<c:forEach begin="1" end="${totalPage }" step="1" var="cnt">
+					<a href="list.jsp?start=${cnt }">[${cnt }]</a>
+				</c:forEach>
+				
+				<%--다음 버튼. 현 페이지에서 +1. 마지막이면 버튼 비활성화 --%>
+				<c:choose>
+					<c:when test="${start < totalPage }">
+						<button type="button" onclick="location.href='list.jsp?start=${start+1}'">다음</button>
+					</c:when>
+					<c:otherwise>
+						<button type="button" disabled="disabled">다음</button>
+					</c:otherwise>
+				</c:choose>
+				
+				<%--등록 버튼 --%>
+				<button type="button" onclick="location.href='regForm.jsp'">등록</button>
+			</td></tr>
 		
 	</table>
 	
--- JSP06/src/testBoard/TestDAO.java
+++ JSP06/src/testBoard/TestDAO.java
@@ -24,7 +24,7 @@ public class TestDAO {
 	}
 	
 	public ArrayList<TestDTO> list() {
-		String sql = "select * from paging";
+		String sql = "select * from paging order by num desc";
 		ArrayList<TestDTO> listDto = new ArrayList<TestDTO>();
 		try{
 			con = DriverManager.getConnection(url, id, pwd);
@@ -46,9 +46,9 @@ public class TestDAO {
 		return listDto;
 	}
 	
-	public void insert(TestDTO dto) {
+	public void insert(TestDTO dto) {		//test_num.nextval: 가상의 넘버링, 시퀀스로 만든 test_num의 다음 값을 넣어준다는 뜻. 
 		String sql = "insert into Paging(num, title, pdate, count) values(test_num.nextval, ?, sysdate, 0)";
-		ArrayList<TestDTO> listDto = new ArrayList<TestDTO>();
+		
 		try{
 			con = DriverManager.getConnection(url, id, pwd);
 			ps = con.prepareStatement(sql);
@@ -59,4 +59,54 @@ public class TestDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	public void count(int num) {
+		String sql = "update paging set count = count+1 where num ="+num;
+		
+		try{
+			con = DriverManager.getConnection(url, id, pwd);
+			ps = con.prepareStatement(sql);
+			ps.executeUpdate();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int getTotalPage() {	//시퀀스 번호를 찾으면(시퀀스는 숫자가 들어갈 때 사이에 몇 개가 빠지는 경우가 종종 발생) 페이지수에 오차가 생기므로
+								//rownum으로 숫자를 찾을 것.
+		String sql = "select count(*) from paging";	//현재 등록된 내용물 총 갯수(4행이면 4가 나옴)
+		int totalPage = 0;
+		try{
+			con = DriverManager.getConnection(url, id, pwd);
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			if(rs.next()) totalPage = rs.getInt(1);	//인덱스 1인 NUM의 총 갯수를 가져와(등록된 내용물 갯수) totalPage에 넣고 반환
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return totalPage;
+	}
 }
+
+/* 생성한 sql 구문
+create table paging(num number not null,
+title varchar2(30) not null,
+pdate varchar2(10) not null,
+count number not null,
+primary key(num));
+
+create sequence test_num;
+*/
+
+/*sql 별칭 주는 방법
+select a.num from(select * from paging order by num desc)a where num between 1 and 3;
+(모든 값을 내림차순으로 가져온다)의 결과가 a에 들어가고 -> a.num으로 a를 쓴다. => a의 값을 1~3 사이에 있는 num만 보겠다
+*/
+
+/*최종적으로 사용할, 페이징을 위한 sql문.. rn은 rownum에게 붙여준 별칭.
+select B.* from(select rownum rn, A.* from(
+  select * from paging order by num desc)A)B	num을 내림차순으로 정렬하여 모든 값을 가져와서 A에 저장
+    where rn between 변수명 and 변수명;				rownum을 변수명~변수명 사이의 결과만 보여달라
+*/
